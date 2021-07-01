d891467f1702c4dc0c861149e0796b59b364dbd8
soso-light <80017031+soso-light@users.noreply.github.com>
2021-07-01 13:37:17 +0900

    complete year (main2)

--- app/src/main/java/com/example/hello/MainActivity2.java
+++ app/src/main/java/com/example/hello/MainActivity2.java
@@ -3,12 +3,33 @@ package com.example.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+    private EditText et;
+    private EditText et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        et = findViewById(R.id.etAge);
+        et2 = findViewById(R.id.etYear);
+    }
+
+    public void onA(View view) {
+        //나이 가져오기
+        int age = Integer.parseInt(et.getText().toString());
+        //나이를 생년으로
+        //나이를 토스트로 띄우기
+    }
+
+    public void onY(View view) {
+        String year = et2.getText().toString();
+        int iYear = 2021 - Integer.parseInt(year);
+        Toast.makeText(this, iYear+ "", Toast.LENGTH_SHORT).show();
     }
 }
