17b7b939bc6dac9640d9f312389ac9afdcce0662
soso-light <80017031+soso-light@users.noreply.github.com>
2021-07-01 11:59:06 +0900

    Toast

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout);
     }
+
+    public void onToastClick(View view){
+        //메박 띄우기
+        Toast.makeText(this, "hello Toast", Toast.LENGTH_SHORT).show();
+    }
+    public void onCountClick(View view){
+        //메박 띄우기
+        Toast.makeText(this, "hello Count..", Toast.LENGTH_SHORT).show();
+    }
+
 }
