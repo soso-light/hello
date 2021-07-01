87d9d2ebb188f13f9227471f6e5f82d31dc0512c
soso-light <80017031+soso-light@users.noreply.github.com>
2021-07-01 13:09:26 +0900

    count up

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int count =0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+
+        textView = findViewById(R.id.txtCount);
     }
 
     public void onToastClick(View view){
@@ -19,8 +24,10 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "hello Toast", Toast.LENGTH_SHORT).show();
     }
     public void onCountClick(View view){
-        //메박 띄우기
-        Toast.makeText(this, "hello Count..", Toast.LENGTH_SHORT).show();
+        //메박 띄우기 ++ , textview에 보이기
+        //Toast.makeText(this, "hello Count..", Toast.LENGTH_SHORT).show();
+        count++;
+        textView.setText(count + "");
     }
 
 }
